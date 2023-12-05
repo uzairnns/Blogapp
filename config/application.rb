fe417a65ebd53bb9727f781e165b5b33bfe6c900
ali uzair <uzairnns5@gmail.com>
2023-12-05 16:17:53 +0500

    config hosts ip

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module Testproject
     config.assets.initialize_on_precompile = false
     config.time_zone = 'Asia/Karachi'
     config.hosts << '51.20.92.68'
-
+    config.hosts << 'https://0.0.0.0:3000'.to_s.gsub('https://', '')
+    config.hosts << '.*'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
