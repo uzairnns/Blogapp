13f451b8d2cdeacf3f46c14cc075adb45afe5a36
ali uzair <uzairnns5@gmail.com>
2023-12-05 16:03:45 +0500

    config hosts

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Testproject
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
     config.time_zone = 'Asia/Karachi'
+    config.hosts << '51.20.92.68'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
